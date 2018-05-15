60bfc4d2ecbd3fa427511f230261967b6ab89949
Fox Foster <fox@tardis.ed.ac.uk>
2018-05-15 14:23:59 +0100

    Initial commit

---
+++ LgramModel.py
@@ -0,0 +1,35 @@
+from nltk.probability import WittenBellProbDist
+from .ngram import NgramModel
+
+
+class LgramModel(NgramModel):
+
+    @staticmethod
+    def _estimator(fdist, bins):
+        """
+        Default estimator function using WB.
+        """
+        # can't be an instance method of NgramModel as they
+        # can't be pickled either.
+        res = WittenBellProbDist(fdist, fdist.B() + 1)
+        res.check()
+        return res
+
+    def __init__(self, n, train, pad_left=False, pad_right=False,
+                 estimator=None, *estimator_args, **estimator_kwargs):
+        """
+        NgramModel (q.v.) slightly tweaked to produce char-grams,
+        not word-grams, with a WittenBell default estimator
+
+        :param train: List of strings, which will be converted to list of lists of characters, but more efficiently
+        :type train: iter(str)
+        """
+        if estimator is None:
+            assert (not (estimator_args)) and (not (estimator_kwargs)), \
+                "estimator_args (%s) or _kwargs (%s) supplied, but no estimator" % (estimator_args, estimator_kwargs)
+            estimator = self._estimator
+        super(LgramModel, self).__init__(n,
+                                         (iter(word) for word in train),
+                                         pad_left, pad_right,
+                                         estimator,
+                                         *estimator_args, **estimator_kwargs)
---
+++ readme.md
@@ -0,0 +1,24 @@
+# The NLTK Model module
+
+The module in `ntlk.model` was removed in NLTK version 3, however it provides some very helpful
+code for text analysis. Although [there are efforts](https://github.com/nltk/nltk/issues/1342) to
+resuscitate the module, it is still not present in NLTK version 3.3 -- and 
+[the nltk/model branch](https://github.com/nltk/nltk/tree/model) has not been active in several years.
+
+As such, I am adapting the model code from [an older version of NLTK](https://github.com/nltk/nltk/blob/2.0.4/).
+Alternatively, one could just install and use NLTK version 2.0.4. However, hopefully, the `nltk.model` will return
+to the upstream.
+
+This code is (barely) modified so that the imports resolve; there are no substantive changes to the code
+other than updating for Python 3 and referencing new NLTK method names. However, I have also included the LgramModel
+from even earlier versions of the NLTK, simply because it is useful in my use-case.
+
+I have browsed the NLTK repositories in the hope of finding where the LgramModel was removed from the tree, but I
+had no luck (it is before the project was using git, I believe).
+
+It retains its original copyright licence - see LICENCE.txt.
+
+Please note that this *still* depends on NLTK as I am not distributing the entire NLTK package -- only a module
+re-adding files from the old model module. You can install the latest version of NLTK through pip: `pip install nltk`.
+
+
